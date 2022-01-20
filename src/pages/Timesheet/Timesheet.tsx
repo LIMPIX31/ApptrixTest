91828db74ef947daf86e83761fb0fac2a57c485d
LIMPIX31 <limpix31@gmail.com>
2022-01-20 17:25:33 +0300

    5. PDF

--- src/pages/Timesheet/Timesheet.tsx
+++ src/pages/Timesheet/Timesheet.tsx
@@ -8,6 +8,9 @@ import { ApptrixApi } from '../../app/abstracts/interfaces/ApptrixApi.interface'
 import { useAuthRedirect } from '../../hooks/useAuthRedirect'
 import cn from 'classnames'
 import { observer } from 'mobx-react'
+import { Document, Page, PDFDownloadLink } from '@react-pdf/renderer'
+import { InclinedButton } from '../../components/UIElements/InclinedButton/InclinedButton'
+import { DataTableCell, Table, TableBody, TableCell, TableHeader } from '@david.kucsai/react-pdf-table'
 
 export const Timesheet: React.FC = () => {
 
@@ -30,15 +33,20 @@ export const Timesheet: React.FC = () => {
         <div className={s.item}>UserName</div>
         <div className={s.item}>Duration</div>
       </div>
-      <WorkItems ytrApt={ytrApi} issueid={issueid || ''}/>
+      <WorkItems ytrApi={ytrApi} issueid={issueid || ''} />
     </div>
+    <PDFDownloadLink document={<PDF ytrApi={ytrApi} issueid={issueid || ''} />}
+                     fileName='timesheet.pdf'>
+      {({ blob, url, loading, error }) => (loading ?
+        <InclinedButton disabled label={'Exporting...'} /> : <InclinedButton label={'Export to PDF'} />)}
+    </PDFDownloadLink>
   </div>
 }
 
 
-export const WorkItems = observer(({ ytrApt, issueid }: { ytrApt: YouTrackApi, issueid: string }) => {
+export const WorkItems = observer(({ ytrApi, issueid }: { ytrApi: YouTrackApi, issueid: string }) => {
   return <>
-    {ytrApt.getWorkitemsFromIssue(issueid).map(wi => (
+    {ytrApi.getWorkitemsFromIssue(issueid).map(wi => (
       <div className={s.row}>
         <div className={s.item}>{wi.author.fullName}</div>
         <div className={s.item}>{formatTime(wi.duration.minutes)}</div>
@@ -50,11 +58,33 @@ export const WorkItems = observer(({ ytrApt, issueid }: { ytrApt: YouTrackApi, i
 const formatTime = (totalMinutes: number) => {
   const hours = Math.floor(totalMinutes / 60)
   const minutes = totalMinutes % 60
-  if(hours > 0 && minutes > 0){
+  if (hours > 0 && minutes > 0) {
     return `${hours} hours ${minutes} minutes`
-  }else if(hours > 0 && minutes === 0){
+  } else if (hours > 0 && minutes === 0) {
     return `${hours} hours`
-  }else if(minutes > 0 && hours === 0){
+  } else if (minutes > 0 && hours === 0) {
     return `${minutes} minutes`
   }
 }
+const PDF = observer(({ ytrApi, issueid }: { ytrApi: YouTrackApi, issueid: string }) => (
+  <Document>
+    <Page size='A4'>
+      <Table
+        data={ytrApi.getWorkitemsFromIssue(issueid)}
+      >
+        <TableHeader>
+          <TableCell>
+            UserName
+          </TableCell>
+          <TableCell>
+            Duration
+          </TableCell>
+        </TableHeader>
+        <TableBody>
+          <DataTableCell getContent={(r) => r.author.fullName} />
+          <DataTableCell getContent={(r) => formatTime(r.duration.minutes)} />
+        </TableBody>
+      </Table>
+    </Page>
+  </Document>
+))
